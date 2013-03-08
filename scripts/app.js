d817d8883780860d7b9c23c981a187569cd0cab9
Michiel Kalkman <michiel@etryx.com>
2013-03-08 18:05:28 +0000

    First Github Pages commit

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+(function() {
+
+  $(function() {
+    var board, run;
+    board = new GameOfLifeBoard({
+      height: 35,
+      width: 85
+    });
+    board.randomize();
+    run = function() {
+      board.printBoard();
+      board.cycle();
+      return $("#gameoflife").html("<pre>" + board._boardBuffer + "</pre>");
+    };
+    return setInterval(run, 200);
+  });
+
+}).call(this);
